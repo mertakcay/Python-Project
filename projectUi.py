5fb1c5b05234ea4994baf70c5687449f271b0b57
Mert <akcaymer1@gmail.com>
2021-11-08 19:21:47 +0300

    update

--- projectUi.py
+++ projectUi.py
@@ -20,16 +20,16 @@ from skimage.morphology import thin
 import numpy as np
 
 class Ui_MainWindow(object):
-    class VideoThread(QThread):
-    change_pixmap_signal = pyqtSignal(np.ndarray)
+    # class VideoThread(QThread):
+    # change_pixmap_signal = pyqtSignal(np.ndarray)
 
-    def run(self):
-        # capture from web cam
-        cap = cv2.VideoCapture(0)
-        while True:
-            ret, cv_img = cap.read()
-            if ret:
-                self.change_pixmap_signal.emit(cv_img)
+    # def run(self):
+    #     # capture from web cam
+    #     cap = cv2.VideoCapture(0)
+    #     while True:
+    #         ret, cv_img = cap.read()
+    #         if ret:
+    #             self.change_pixmap_signal.emit(cv_img)
 
 
     path = ''
